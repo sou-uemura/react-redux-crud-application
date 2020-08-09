ae85b5fae5ce80ffba329ecc2378ad192cbd8355
sou-uemura <soh.mtw@gmail.com>
2020-08-09 16:10:29 +0700

    remove dead code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import React from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("I am clicked!")}}/>
-//       </React.Fragment>
-//     )
-//   }
-// }
-
 const App = () => {
   return (
     <div>
